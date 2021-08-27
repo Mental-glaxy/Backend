3dfb815e98409770509632942728f863a529af86
Vladimir <v.ivahnenko@outlook.com>
2021-08-27 19:02:17 +0300

    OP15-31 поставил заглушку для middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AdminModule } from "./admin/admin.module";
+import { AdminMiddleware } from "./admin.middleware";
 @Module({
   imports: [
     AuthModule,
@@ -13,4 +19,11 @@ import { AdminModule } from "./admin/admin.module";
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AdminMiddleware)
+      .exclude({ path: "admin/send-invitation", method: RequestMethod.POST })
+      .forRoutes("/admin");
+  }
+}
